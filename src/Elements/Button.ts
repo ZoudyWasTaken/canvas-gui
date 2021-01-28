276b83ae17ca5ca9348c648e05cdc92bc63bda86
ZoudyWasTaken <itzfoxly@gmail.com>
2021-01-28 01:55:20 -0500

    Update Button.ts

--- src/Elements/Button.ts
+++ src/Elements/Button.ts
@@ -1,28 +1,36 @@
-import draw_text from "Utils/text";
+import TextUtil from "Utils/TextUtil";
+import { Rect, Position } from "Utils/Util";
 import Element from "./Element";
 export default class Button extends Element {
-    text: string;
-    constructor(context: CanvasRenderingContext2D, text: string, x: number, y: number, width: number, height: number) {
-        super(context, x, y, width, height);
-        this.text = text;
-        this.render(context);
+    innerText: string;
+    constructor(context: CanvasRenderingContext2D, text: string, size: Rect, pos: Position) {
+        super(context, {
+            width: size.width, 
+            height: size.height
+        }, pos);
+        this.innerText = text;
+        this.render(context, this.pos);
     }
 
-    render(context: CanvasRenderingContext2D | null): void {
+    render(context: CanvasRenderingContext2D | null, pos: Position = this.pos): void {
         const prev_fillstyle = context.fillStyle;
 
-        if (this.inside(this, 170, 96)) {
-            context.fillStyle = "darkblue";
-        } else {
-            context.fillStyle = "aqua";
-        }        
-        context.fillRect(this.x - (this.width / 32), this.y + (this.height / 8), this.width, this.height);
+        // if (this.inside(this, { x: 170, y: 96 })) {
+        //     context.fillStyle = "darkblue";
+        // } else {
+
+        context.fillStyle = "aqua"; 
+        context.fillRect(pos.x - (this.size.width / 32), pos.y + (this.size.height / 8), this.size.width, this.size.height);
 
         context.fillStyle = "blue";
-        context.fillRect(this.x, this.y, this.width, this.height);
+        context.fillRect(pos.x, pos.y, this.size.width, this.size.height);
 
         context.fillStyle = prev_fillstyle;
-        draw_text(context, this.text, (this.x + this.width / 5), (this.y + this.height));
+        TextUtil.draw_text(context, this.innerText, { 
+            x: (pos.x + this.size.width / 5), 
+            y: (pos.y + this.size.height) 
+        });
+        
         return;
     }
 
